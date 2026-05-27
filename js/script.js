02bcb7ac06d8e9595833e1d2513761e37c233ef6
Greg Dev <cimmerian422@gmail.com>
2026-05-27 14:07:09 +0200

    js job

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let computerMove = `kamień`;
+
+let playerMove = 'papier'
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
