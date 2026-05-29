756887bb35de56d6c54bdaf72a920fee49d2bdf5
Greg Dev <cimmerian422@gmail.com>
2026-05-29 12:36:27 +0200

    tworzenie gry

--- js/script.js
+++ js/script.js
@@ -1,7 +1,47 @@
-let computerMove = `kamień`;
+// Losowanie ruchu komputera
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-let computerMove = `kamień`;
+let computerMove = 'nieznany ruch';
 
-let playerMove = 'papier'
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+if (randomNumber == 1) {
+    computerMove = 'kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'papier';
+} else if (randomNumber == 3) {
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+// Pobranie i odczytanie ruchu gracza
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+// LOGIKA WYNIKU GRY
+
+if (playerMove == 'nieznany ruch') {
+    printMessage('Wpisałeś niepoprawną liczbę! Wybierz 1, 2 lub 3.');
+} else if (playerMove == computerMove) {
+    printMessage('Remis!');
+} else if (
+    (playerMove == 'papier' && computerMove == 'kamień') ||
+    (playerMove == 'kamień' && computerMove == 'nożyce') ||
+    (playerMove == 'nożyce' && computerMove == 'papier')
+) {
+    printMessage('Ty wygrywasz!');
+} else {
+    printMessage('Przegrywasz! Komputer był lepszy.');
+}
