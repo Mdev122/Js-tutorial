a8791c86e8756724bffb86e7250f5948a194dd03
Greg Dev <cimmerian422@gmail.com>
2026-06-02 09:57:18 +0200

    Dodanie listenerów

--- js/script.js
+++ js/script.js
@@ -52,3 +52,15 @@ function playGame(playerInput) {
     // Wywołanie funkcji sprawdzającej wynik
     displayResult(computerMove, playerMove);
 }
+// Słuchacze zdarzeń dla przycisków gry
+document.getElementById('play-rock').addEventListener('click', function() {
+    playGame(1);
+});
+
+document.getElementById('play-paper').addEventListener('click', function() {
+    playGame(2);
+});
+
+document.getElementById('play-scissors').addEventListener('click', function() {
+    playGame(3);
+});
